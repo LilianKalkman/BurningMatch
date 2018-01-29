a79f3b3ae69cd31c933493abbee9b19db585920b
Schmarfy <o.vanzijverden@gmail.com>
2018-01-29 14:37:12 +0100

    Add User model validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "is invalid when no email is given" do
+      user = User.new(email: "", password: "123456")
+      user.valid?
+      expect(user.errors).to have_key(:email)
+    end
+  end
 end
