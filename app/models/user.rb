0c60467dd0d36499f6818199cebca6b1640a0fb3
Schmarfy <o.vanzijverden@gmail.com>
2018-01-29 15:47:01 +0100

    Add testing for email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
   validates :password, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,15 +3,31 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "validations" do
     it "is invalid when no email is given" do
-      user = User.new(email: "", password: "123456")
-      user.valid?
-      expect(user.errors).to have_key(:email)
+      set_user("","123456")
+      @user.valid?
+      expect(@user.errors).to have_key(:email)
     end
 
-    it "is invalid when no email is given" do
-      user = User.new(email: "hello@hello.com", password: "")
-      user.valid?
-      expect(user.errors).to have_key(:password)
+    it "is invalid when no password is given" do
+      set_user("mail@mail.com", "")
+      @user.valid?
+      expect(@user.errors).to have_key(:password)
     end
+
+  # it "is invalid when email already exists" do
+  #   set_user("mail@mail.com", "123456")
+  #   user2 = @user.dup
+  #   # user_with_same_email = @user.email.downcase
+  #   user2.save
+  #
+  #   expect { user2.save! }.to raise_error
+  #   end
+
+
   end
+
+  def set_user(email, password)
+    @user = User.new(email: email, password: password)
+  end
+
 end
