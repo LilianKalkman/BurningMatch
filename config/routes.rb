3b26eb48e23da0e6f29a8713e49d877233ff68f0
Schmarfy <o.vanzijverden@gmail.com>
2018-01-30 13:10:03 +0100

    Add API routes & API Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
 
   root to: "pages#home"
   devise_for :users
+  resources :users
+
+  # namespace :api do
+  #   resources :users, only: [:update]
+  # end
 
 end
