54b70ecfbe1fb39ec2cfa384a77fd1dba5e57652
LilianKalkman <liliankalkman@hotmail.com>
2018-02-01 16:39:33 +0100

    navbar tests workingggg

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    email    { Faker::Internet.email }
+    password { Faker::Internet.password }
+    admin false
+  end
+
+  factory :admin, class: User do
+    email    { Faker::Internet.email }
+    password { Faker::Internet.password }
+    admin true
   end
 end
