f8232bd2e735279518fefa5c5464bc15b67ee302
LilianKalkman <liliankalkman@hotmail.com>
2018-02-01 17:47:53 +0100

    good evening

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -7,10 +7,19 @@ describe "navigations for students" do
 
   it "allows navigating from root page to my matches" do
     visit root_url
-
     click_link "My Matches"
-
     expect(current_path).to eq(mymatches_path)
   end
+end
 
+
+describe "navigations for admin" do
+  before {login_as user}
+  let(:user) { create :user, email: "Arno@school.com", password: "123456", admin: true }
+
+  it "navigates to student matches" do
+    visit root_url
+    click_link "Student Matches"
+    expect(current_path).to eq(matches_path)
+  end
 end
